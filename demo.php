2ff01c5c273f32e2530f83b3bfb30e9b86101d57
aisuhua <1079087531@qq.com>
2016-08-01 16:30:12 +0800

    update

---
+++ demo.php
@@ -0,0 +1,208 @@
+<?php
+$str = 'Contents at a Glance	4
+Contents	6
+About the Author	16
+About the Technical Reviewer	18
+Introduction	20
+Chapter 1: Using PHP	0002721529.INDD:b978 1 4842 1922 5_1:10
+	Embedding PHP	0002721529.INDD:Sec1:11
+	Outputting Text	0002721529.INDD:Sec2:12
+	Installing a Web Server	0002721529.INDD:Sec3:13
+	Hello World	0002721529.INDD:Sec4:14
+	Compile and Parse	0002721529.INDD:Sec5:15
+	Comments	0002721529.INDD:Sec6:16
+Chapter 2: Variables	0002721530.INDD:b978 1 4842 1922 5_2:2
+	Defining Variables	0002721530.INDD:Sec1:3
+	Data Types	0002721530.INDD:Sec2:4
+	Integer Type	0002721530.INDD:Sec3:5
+	Floating-Point Type	0002721530.INDD:Sec4:6
+	Bool Type	0002721530.INDD:Sec5:7
+	Null Type	0002721530.INDD:Sec6:8
+	Default Values	0002721530.INDD:Sec7:9
+Chapter 3: Operators	0002721531.INDD:b978 1 4842 1922 5_3:2
+	Arithmetic Operators	0002721531.INDD:Sec1:3
+	Assignment Operators	0002721531.INDD:Sec2:4
+	Combined Assignment Operators	0002721531.INDD:Sec3:5
+	Increment and Decrement Operators	0002721531.INDD:Sec4:6
+	Comparison Operators	0002721531.INDD:Sec5:7
+	Logical Operators	0002721531.INDD:Sec6:8
+	Bitwise Operators	0002721531.INDD:Sec7:9
+	Operator Precedence	0002721531.INDD:Sec8:10
+	Additional Logical Operators	0002721531.INDD:Sec9:11
+Chapter 4: String	0002721532.INDD:b978 1 4842 1922 5_4:2
+	String Concatenation	0002721532.INDD:Sec1:3
+	Delimiting Strings	0002721532.INDD:Sec2:4
+	Heredoc Strings	0002721532.INDD:Sec3:5
+	Nowdoc Strings	0002721532.INDD:Sec4:6
+	Escape Characters	0002721532.INDD:Sec5:7
+	Character Reference	0002721532.INDD:Sec6:8
+	String Compare	0002721532.INDD:Sec7:9
+Chapter 5: Arrays	0002721533.INDD:b978 1 4842 1922 5_5:1
+	Numeric Arrays	0002721533.INDD:Sec1:2
+	Associative Arrays	0002721533.INDD:Sec2:3
+	Mixed Arrays	0002721533.INDD:Sec3:4
+	Multi-Dimensional Arrays	0002721533.INDD:Sec4:5
+Chapter 6: Conditionals	0002721534.INDD:b978 1 4842 1922 5_6:1
+	If Statement	0002721534.INDD:Sec1:2
+	Switch Statement	0002721534.INDD:Sec2:3
+	Alternative Syntax	0002721534.INDD:Sec3:4
+	Mixed Modes	0002721534.INDD:Sec4:5
+	Ternary Operator	0002721534.INDD:Sec5:6
+Chapter 7: Loops	0002721535.INDD:b978 1 4842 1922 5_7:1
+	While Loop	0002721535.INDD:Sec1:2
+	Do-while Loop	0002721535.INDD:Sec2:3
+	For Loop	0002721535.INDD:Sec3:4
+	Foreach Loop	0002721535.INDD:Sec4:5
+	Alternative Syntax	0002721535.INDD:Sec5:6
+	Break	0002721535.INDD:Sec6:7
+	Continue	0002721535.INDD:Sec7:8
+	Goto	0002721535.INDD:Sec8:9
+Chapter 8: Functions	0002721536.INDD:b978 1 4842 1922 5_8:5
+	Defining Functions	0002721536.INDD:Sec1:6
+	Calling Functions	0002721536.INDD:Sec2:7
+	Function Parameters	0002721536.INDD:Sec3:8
+	Default Parameters	0002721536.INDD:Sec4:9
+	Variable Parameter Lists	0002721536.INDD:Sec5:10
+	Return Statement	0002721536.INDD:Sec6:11
+	Scope and Lifetime	0002721536.INDD:Sec7:12
+	Anonymous Functions	0002721536.INDD:Sec8:13
+	Closures	0002721536.INDD:Sec9:14
+	Generators	0002721536.INDD:Sec10:15
+	Built-in Functions	0002721536.INDD:Sec11:16
+Chapter 9: Class	0002721537.INDD:b978 1 4842 1922 5_9:1
+	Instantiating an Object	0002721537.INDD:Sec1:2
+	Accessing Object Members	0002721537.INDD:Sec2:3
+	Initial Property Values	0002721537.INDD:Sec3:4
+	Constructor	0002721537.INDD:Sec4:5
+	Destructor	0002721537.INDD:Sec5:6
+	Case Sensitivity	0002721537.INDD:Sec6:7
+	Object Comparison	0002721537.INDD:Sec7:8
+	Anonymous Classes	0002721537.INDD:Sec8:9
+	Closure Object	0002721537.INDD:Sec9:10
+Chapter 10: Inheritance	0002721538.INDD:b978 1 4842 1922 5_10:1
+	Overriding Members	0002721538.INDD:Sec1:2
+	Final Keyword	0002721538.INDD:Sec2:3
+	Instanceof Operator	0002721538.INDD:Sec3:4
+Chapter 11: Access Levels	0002721539.INDD:b978 1 4842 1922 5_11:1
+	Private Access	0002721539.INDD:Sec1:2
+	Protected Access	0002721539.INDD:Sec2:3
+	Public Access	0002721539.INDD:Sec3:4
+	Var Keyword	0002721539.INDD:Sec4:5
+	Object Access	0002721539.INDD:Sec5:6
+	Access Level Guideline	0002721539.INDD:Sec6:7
+Chapter 12: Static	0002721540.INDD:b978 1 4842 1922 5_12:1
+	Referencing Static Members	0002721540.INDD:Sec1:2
+	Static Variables	0002721540.INDD:Sec2:3
+	Late Static Bindings	0002721540.INDD:Sec3:4
+Chapter 13: Constants	0002721541.INDD:b978 1 4842 1922 5_13:2
+	Const	0002721541.INDD:Sec1:3
+	Define	0002721541.INDD:Sec2:4
+	Const and define	0002721541.INDD:Sec3:5
+	Constant Guideline	0002721541.INDD:Sec4:6
+	Magic Constants	0002721541.INDD:Sec5:7
+Chapter 14: Interface	0002721542.INDD:b978 1 4842 1922 5_14:1
+	Interface Signatures	0002721542.INDD:Sec1:2
+	Interface Example	0002721542.INDD:Sec2:3
+	Interface Usages	0002721542.INDD:Sec3:4
+	Interface Guideline	0002721542.INDD:Sec4:5
+Chapter 15: Abstract	0002721543.INDD:b978 1 4842 1922 5_15:1
+	Abstract Methods	0002721543.INDD:Sec1:2
+	Abstract Example	0002721543.INDD:Sec2:3
+	Abstract Classes and Interfaces	0002721543.INDD:Sec3:4
+	Abstract Guideline	0002721543.INDD:Sec4:5
+Chapter 16: Traits	0002721544.INDD:b978 1 4842 1922 5_16:1
+	Inheritance and Traits	0002721544.INDD:Sec1:2
+	Trait Guidelines	0002721544.INDD:Sec2:3
+Chapter 17: Importing Files	0002721545.INDD:b978 1 4842 1922 5_17:3
+	Include Path	0002721545.INDD:Sec1:4
+	Require	0002721545.INDD:Sec2:5
+	Include_once	0002721545.INDD:Sec3:6
+	Require_once	0002721545.INDD:Sec4:7
+	Return	0002721545.INDD:Sec5:8
+	_Autoload	0002721545.INDD:Sec6:9
+Chapter 18: Type Declarations	0002721546.INDD:b978 1 4842 1922 5_18:2
+	Argument Type Declarations	0002721546.INDD:Sec1:3
+	Return Type Declarations	0002721546.INDD:Sec2:4
+	Strict Typing	0002721546.INDD:Sec3:5
+Chapter 19: Type Conversions	0002721547.INDD:b978 1 4842 1922 5_19:2
+	Explicit Casts	0002721547.INDD:Sec1:3
+	Set type	0002721547.INDD:Sec2:4
+	Get type	0002721547.INDD:Sec3:5
+Chapter 20: Variable Testing	0002721548.INDD:b978 1 4842 1922 5_20:2
+	Isset	0002721548.INDD:Sec1:3
+	Empty	0002721548.INDD:Sec2:4
+	Is_null	0002721548.INDD:Sec3:5
+	Unset	0002721548.INDD:Sec4:6
+	Null Coalescing Operator	0002721548.INDD:Sec5:7
+	Determining Types	0002721548.INDD:Sec6:8
+	Variable Information	0002721548.INDD:Sec7:9
+Chapter 21: Overloading	0002721549.INDD:b978 1 4842 1922 5_21:1
+	Property Overloading	0002721549.INDD:Sec1:2
+	Method Overloading	0002721549.INDD:Sec2:3
+	Isset and unset Overloading	0002721549.INDD:Sec3:4
+Chapter 22: Magic Methods	0002721550.INDD:b978 1 4842 1922 5_22:2
+	_ToString	0002721550.INDD:Sec1:3
+	_Invoke	0002721550.INDD:Sec2:4
+	Object Serialization	0002721550.INDD:Sec3:5
+	_Sleep	0002721550.INDD:Sec4:6
+	_Wakeup	0002721550.INDD:Sec5:7
+	Set State	0002721550.INDD:Sec6:8
+	Object Cloning	0002721550.INDD:Sec7:9
+Chapter 23: User Input	0002721551.INDD:b978 1 4842 1922 5_23:5
+	HTML Form	0002721551.INDD:Sec1:6
+	Sending with POST	0002721551.INDD:Sec2:7
+	Sending with GET	0002721551.INDD:Sec3:8
+	Request Array	0002721551.INDD:Sec4:9
+	Security Concerns	0002721551.INDD:Sec5:10
+	Submitting Arrays	0002721551.INDD:Sec6:11
+	File Uploading	0002721551.INDD:Sec7:12
+	Superglobals	0002721551.INDD:Sec8:13
+Chapter 24: Cookies	0002721552.INDD:b978 1 4842 1922 5_24:1
+	Creating Cookies	0002721552.INDD:Sec1:2
+	Cookie Array	0002721552.INDD:Sec2:3
+	Deleting Cookies	0002721552.INDD:Sec3:4
+Chapter 25: Sessions	0002721553.INDD:b978 1 4842 1922 5_25:1
+	Starting a Session	0002721553.INDD:Sec1:2
+	Session Array	0002721553.INDD:Sec2:3
+	Deleting a Session	0002721553.INDD:Sec3:4
+Chapter 26: Namespaces	0002721554.INDD:b978 1 4842 1922 5_26:1
+	Creating Namespaces	0002721554.INDD:Sec1:2
+	Nested Namespaces	0002721554.INDD:Sec2:3
+	Alternative Syntax	0002721554.INDD:Sec3:4
+	Referencing Namespaces	0002721554.INDD:Sec4:5
+	Namespace Aliases	0002721554.INDD:Sec5:6
+	Namespace Keyword	0002721554.INDD:Sec6:7
+	Namespace Guideline	0002721554.INDD:Sec7:8
+Chapter 27: References	0002721555.INDD:b978 1 4842 1922 5_27:1
+	Assign by Reference	0002721555.INDD:Sec1:2
+	Pass by Reference	0002721555.INDD:Sec2:3
+	Return by Reference	0002721555.INDD:Sec3:4
+Chapter 28: Advanced Variables	0002721556.INDD:b978 1 4842 1922 5_28:1
+	Curly Syntax	0002721556.INDD:Sec1:2
+	Variable Variable Names	0002721556.INDD:Sec2:3
+	Variable Function Names	0002721556.INDD:Sec3:4
+	Variable Class Names	0002721556.INDD:Sec4:5
+Chapter 29: Error Handling	0002721557.INDD:b978 1 4842 1922 5_29:4
+	Correcting Errors	0002721557.INDD:Sec1:5
+	Error Levels	0002721557.INDD:Sec2:6
+	Error-Handling Environment	0002721557.INDD:Sec3:7
+	Custom Error Handlers	0002721557.INDD:Sec4:8
+	Raising Errors	0002721557.INDD:Sec5:9
+Chapter 30: Exception Handling	0002721558.INDD:b978 1 4842 1922 5_30:3
+	Try-catch Statement	0002721558.INDD:Sec1:4
+	Throwing Exceptions	0002721558.INDD:Sec2:5
+	Catch Block	0002721558.INDD:Sec3:6
+	Finally Block	0002721558.INDD:Sec4:7
+	Rethrowing Exceptions	0002721558.INDD:Sec5:8
+	Uncaught Exception Handler	0002721558.INDD:Sec6:9
+	Errors and Exceptions	0002721558.INDD:Sec7:10
+Chapter 31: Assertions	0002721559.INDD:b978 1 4842 1922 5_31:1
+	Assert Performance	0002721559.INDD:Sec1:2
+Index	0002721561.INDD:Ind1:1
+';
+
+
+$pattern = '/(.*)\s+000(.*)(\n)/';
+$replacement = '${1}${3}';
+
+echo (preg_replace($pattern, $replacement, $str));
